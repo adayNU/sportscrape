bc1c00d4313b151780edf1b224b3dc265f9198f3
aday <aday@liveramp.com>
2018-02-25 17:33:51 -0800

    tabs => spaces

--- main.rb
+++ main.rb
@@ -12,44 +12,44 @@ DATE_COLUMN = "Date"
 FIFTY_MILES_IN_METERS = 80467.2
 
 def run!
-	SPORTS.each do |sport|
-		# I had to modify the request since nusports returns
-		# 404 for some strange reason (presumably to prevent
-		# scraping, though a 5xx error seems more appropriate).
-		response = RestClient.get("http://nusports.com/schedule.aspx?path=#{sport}", {user_agent: "go cats"})
+  SPORTS.each do |sport|
+    # I had to modify the request since nusports returns
+    # 404 for some strange reason (presumably to prevent
+    # scraping, though a 5xx error seems more appropriate).
+    response = RestClient.get("http://nusports.com/schedule.aspx?path=#{sport}", {user_agent: "go cats"})
 
-		page = Nokogiri::HTML(response.body)
+    page = Nokogiri::HTML(response.body)
 
-		# grab each row of the schedule table
-		rows = page.css("table#ctl00_cplhMainContent_dgrdschedule tr")
+    # grab each row of the schedule table
+    rows = page.css("table#ctl00_cplhMainContent_dgrdschedule tr")
 
-		sport_name = page.css(".sports-nav-level-1-link").first.children.first.text
-		header_row_text = rows.shift.children.map{|e| e.text.strip}
-		
-		location_column = header_row_text.index(LOCATION_COLUMN)
-		date_column = header_row_text.index(DATE_COLUMN)
+    sport_name = page.css(".sports-nav-level-1-link").first.children.first.text
+    header_row_text = rows.shift.children.map{|e| e.text.strip}
 
-		rows.each_with_index do |game|
-			location = game.children[location_column].text
-			date = Date.new
+    location_column = header_row_text.index(LOCATION_COLUMN)
+    date_column = header_row_text.index(DATE_COLUMN)
 
-			begin
-				date = Date.strptime(game.children[date_column].text.strip, "%m/%d/%Y")
-			rescue
-				# Oh well, guess we aren't going to this game
-			end
+    rows.each_with_index do |game|
+      location = game.children[location_column].text
+      date = Date.new
 
-			# Only check events in CA to avoid unnecessary queries to
-			# the Google Maps API
-			if location.include? STATE
-				resp = RestClient.get(GOOGLE_MAPS_URL + location.gsub(/[^0-9a-z ]/i, '').gsub(" ", "+"))
-				distance = JSON.parse(resp.body)["rows"].first["elements"].first["distance"]["value"]
-				if distance < FIFTY_MILES_IN_METERS && date > Date.today
-					puts "#{date}: #{sport_name} - #{location}"
-				end
-			end
-		end
-	end
+      begin
+        date = Date.strptime(game.children[date_column].text.strip, "%m/%d/%Y")
+      rescue
+      # Oh well, guess we aren't going to this game
+      end
+
+      # Only check events in CA to avoid unnecessary queries to
+      # the Google Maps API
+      if location.include? STATE
+        resp = RestClient.get(GOOGLE_MAPS_URL + location.gsub(/[^0-9a-z ]/i, '').gsub(" ", "+"))
+        distance = JSON.parse(resp.body)["rows"].first["elements"].first["distance"]["value"]
+        if distance < FIFTY_MILES_IN_METERS && date > Date.today
+          puts "#{date}: #{sport_name} - #{location}"
+        end
+      end
+    end
+  end
 end
 
 run!
